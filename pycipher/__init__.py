14e7bc679a7f78e2b28a4f3a4d45b01d570bd89f
mos9527 <31397301+greats3an@users.noreply.github.com>
2022-02-02 23:06:11 +0800

    Update __init__.py

--- pycipher/__init__.py
+++ pycipher/__init__.py
@@ -1,12 +1,10 @@
 from . import cipher
 # Modes (see cipher.cpp)
-MODE_BASE85 = 0b100
-MODE_BASE64 = 0b000
+MODE_BASE64 = 0b100
 MODE_CBC    = 0b010
-MODE_ECB    = 0b000
 MODE_FENCE  = 0b001
-def encrypt(src,password,iv=None,mode = MODE_BASE85 | MODE_CBC | MODE_FENCE):
+def encrypt(src,password,iv=None,mode = MODE_BASE64 | MODE_CBC | MODE_FENCE):
     return cipher.encrypt(src,password,iv or '',mode)
 
-def decrypt(src,password,iv=None,mode = MODE_BASE85 | MODE_CBC | MODE_FENCE):
+def decrypt(src,password,iv=None,mode = MODE_BASE64 | MODE_CBC | MODE_FENCE):
     return cipher.decrypt(src,password,iv or '',mode) 
